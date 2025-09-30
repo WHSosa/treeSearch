5bab21a06d610ba8a46a75c22c111137416c6eae
Werner Sosa <2024161143@ub.edu.bz>
2025-09-30 10:10:53 -0600

    commit main, .gitignore, and README.md

---
+++ treeSearch.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct BinaryTreeNode {
+    string data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+
+    BinaryTreeNode(const string& value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
